b07c40eef8b204fb434d8de7c329e06faf76b2f4
VTheGreek <vasilis.proefth@gmail.com>
2026-03-31 23:56:08 +0300

    Feat: React, Components.jsx, Favorites.jsx, Home.jsx

---
+++ Movie-App/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Home />
+    </>
+  );
+}
+
+export default App
